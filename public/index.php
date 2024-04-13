6b75f73200660b3a80942eef9e4b000a2695a78d
Ben Yafai <ben@yaf.ai>
2024-04-13 22:04:29 +0100

    ✨ Added an RSS feed!

--- public/index.php
+++ public/index.php
@@ -80,6 +80,35 @@ function menu() {
     return $menuItems;
 }
 
+$app->get("/feed", function (Request $request, Response $response, $args) {
+    $rss = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<rss version=\"2.0\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xml:base=\"https://"
+            .    $_SERVER["HTTP_HOST"] . "\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
+    <channel>
+        <title>My Website</title>
+        <link>https://" . $_SERVER["HTTP_HOST"] . "</link>
+        <atom:link href=\"https://" . $_SERVER["HTTP_HOST"] . "/rss\" rel=\"self\" type=\"application/rss+xml\"/>
+        <description>RSS for My Website</description>
+        <language>en-gb</language>";
+        $posts = getPosts(true);
+        foreach ($posts as $post) {
+            $rss .= "
+        <item>
+            <title>" . $post->postMeta->Title . "</title>
+            <pubDate>" . date("r", $post->modified) . "</pubDate>
+            <link>https://" . $_SERVER["HTTP_HOST"] . "/" . $post->file . "</link>
+            <description>" . str_replace("<", "&lt;", str_replace(">", "&gt;", $post->markdown)) . "</description>
+            <dc:creator>Me!</dc:creator>
+            <guid>https://" . $_SERVER["HTTP_HOST"] . "/" . $post->file . "</guid>
+        </item>";
+        }
+        $rss .= "
+    </channel>
+</rss>";
+        $response->getBody()->write($rss);
+        return $response->withHeader("Content-Type", "application/xml");
+});
+
 $app->get("/post/[{post}]", function (Request $request, Response $response, $args) {
     $post = $args["post"] ?? null;
     $post = file_exists("./../posts/$post.md") ? parseFile("./../posts/", $post . ".md") : false;
