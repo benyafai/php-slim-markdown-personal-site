7904750756d0d5c2ce1d63cb0fcd216446ba079c
Ben Yafai <ben@yaf.ai>
2024-04-13 22:41:06 +0100

    ✨ An actual, working, 404 function!

--- public/index.php
+++ public/index.php
@@ -12,10 +12,20 @@ $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-$errorHandler = $errorMiddleware->getDefaultErrorHandler();
-$errorHandler->registerErrorRenderer('text/html', function() {
-    echo '404';
-    exit();
+$errorMiddleware->setErrorHandler(\Slim\Exception\HttpNotFoundException::class, function (
+    \Psr\Http\Message\ServerRequestInterface $request,
+    \Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) {
+    $response = new \Slim\Psr7\Response();
+    return (new PhpRenderer("./../layouts"))->render($response, "layout.php", [
+        "content" => null,
+        "type" => "page",
+        "menus" => menu(),
+        "allPosts" => $showAllPosts ? getPosts() : null,
+    ]);
 });
 
 function getPosts($all = false) {
